6713a8299c2d957ac8fd24f9f05f15d4a53506c5
Taylor Garcia <tclustuff@gmail.com>
2018-08-19 16:10:26 -0700

    Hook up stat ui

---
+++ Assets/Scripts/UI/LabelValue.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LabelValue : MonoBehaviour
+{
+    [SerializeField] private Text _label;
+    [SerializeField] private Text _value;
+
+    public void SetLabel(string text)
+    {
+        _label.text = text;
+    }
+
+    public void SetValue(string text)
+    {
+        _value.text = text;
+    }
+}
