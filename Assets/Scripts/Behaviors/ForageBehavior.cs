99862f2308f308e3349408f38413763ac2e9aa81
Commkeen <commkeen4000@gmail.com>
2018-08-19 16:50:46 -0700

    Script for foraging and studying

--- Assets/Scripts/Behaviors/ForageBehavior.cs
+++ Assets/Scripts/Behaviors/ForageBehavior.cs
@@ -7,9 +7,10 @@ public class ForageBehavior : ActionBehavior
     private void Start()
     {
         var lines = new List<DialogueLine>() {
-            new DialogueLine("Player", "You go out to find some supplies...", null),
-            new DialogueLine("Player", "You root around in the sand for a while...", null),
-            new DialogueLine("Player", "You end up finding some crabs that look like they might taste all right.", null),
+            new DialogueLine("", "I went out to find some supplies..."),
+            new DialogueLine("", "I rooted around in the sand for a while..."),
+            new DialogueLine("", "I ended up finding some crabs that look like they might taste all right."),
+            new DialogueLine("", "I also found some driftwood that might come in handy."),
         };
         _dialogue = new Dialogue(lines, null, () => {
             EndAction();
@@ -29,6 +30,7 @@ public class ForageBehavior : ActionBehavior
     protected override void EndAction()
     {
         GameStatus.Instance.InventoryItems["foodRaw"].Amount += 1;
+        GameStatus.Instance.InventoryItems["wood"].Amount += 1;
         base.EndAction();
     }
 }
--- Assets/Scripts/Behaviors/StudyBehavior.cs
+++ Assets/Scripts/Behaviors/StudyBehavior.cs
@@ -1,20 +1,34 @@
 ﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class StudyBehavior : ActionBehavior
 {
-    private Dialogue _dialogue;
-
-    private void Start()
-    {
-        var lines = new List<DialogueLine>() { new DialogueLine("Player", "I spent some time reading the survival guide.", null) };
-        _dialogue = new Dialogue(lines, null, () => {
-            EndAction();
-        });
-    }
 
     protected override void OnActionStart()
     {
-        SetDialogue(_dialogue);
+        var lines = new List<DialogueLine>() { new DialogueLine("", "I spent some time reading the survival guide.") };
+
+        var roll = Random.Range(0, 3);
+        if (roll < 1)
+        {
+            lines.Add(new DialogueLine("", "I learned some cooking techniques."));
+            GameStatus.Instance.SkillLevels["cooking"].Level += 2;
+        }
+        else if (roll < 2)
+        {
+            lines.Add(new DialogueLine("", "I learned some woodworking techniques."));
+            GameStatus.Instance.SkillLevels["craftsmanship"].Level += 2;
+        }
+        else
+        {
+            lines.Add(new DialogueLine("", "I learned some fishing techniques."));
+            GameStatus.Instance.SkillLevels["fishing"].Level += 2;
+        }
+
+
+        SetDialogue(new Dialogue(lines, null, () => {
+            EndAction();
+        }));
     }
 
     protected override void EndAction()
