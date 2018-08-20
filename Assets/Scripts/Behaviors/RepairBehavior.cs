1417d5f2af684e2d2cd9fcf1bc40696c140e558e
Commkeen <commkeen4000@gmail.com>
2018-08-19 19:00:56 -0700

    Dialog options for repair scenes

--- Assets/Scripts/Behaviors/RepairBehavior.cs
+++ Assets/Scripts/Behaviors/RepairBehavior.cs
@@ -6,36 +6,53 @@ public class RepairBehavior : ActionBehavior
     protected override void OnActionStart()
     {
         var lines = new List<DialogueLine>();
+        var options = new List<DialogueOption>();
         var repairs = GetRepairAmount();
         var currentRepair = GameStatus.Instance.RepairLevel.CurrentValue;
 
         if (currentRepair == 0)
         {
             lines = RepairMilestone1();
+            options = new List<DialogueOption>() {
+                new DialogueOption("You look like you’ve seen better days.", () => { SetDialogue(new Dialogue(RepairMilestone1_OptionA(), null)); }),
+                new DialogueOption("Change the subject", () => { SetDialogue(new Dialogue(RepairMilestone1_OptionB(), null)); }),
+            };
         }
         else if (currentRepair < 20 && currentRepair + repairs >= 20)
         {
             lines = RepairMilestone2();
+            options = null;
         }
         else if (currentRepair < 40 && currentRepair + repairs >= 40)
         {
             lines = RepairMilestone3();
+            options = null;
         }
         else if (currentRepair < 60 && currentRepair + repairs >= 60)
         {
             lines = RepairMilestone4();
+            options = new List<DialogueOption>() {
+                new DialogueOption("Hornbeck was a terrible captain", () => { SetDialogue(new Dialogue(RepairMilestone4_OptionA(), null)); }),
+                new DialogueOption("Hornbeck had to save herself", () => { SetDialogue(new Dialogue(RepairMilestone4_OptionB(), null)); }),
+            };
         }
         else if (currentRepair < 80 && currentRepair + repairs >= 80)
         {
             lines = RepairMilestone5();
+            options = new List<DialogueOption>() {
+                new DialogueOption("Probably", () => { SetDialogue(new Dialogue(RepairMilestone5_OptionA(), null)); }),
+                new DialogueOption("Probably not", () => { SetDialogue(new Dialogue(RepairMilestone5_OptionB(), null)); }),
+                new DialogueOption("Who cares?", () => { SetDialogue(new Dialogue(RepairMilestone5_OptionC(), null)); }),
+            };
         }
         else
         {
             lines = NormalRepair();
+            options = null;
         }
 
 
-        SetDialogue(new Dialogue(lines, null));
+        SetDialogue(new Dialogue(lines, options));
     }
 
     protected override void EndAction()
@@ -85,27 +102,52 @@ public class RepairBehavior : ActionBehavior
         lines.Add(new DialogueLine("Player", "No, but I’ve got an idea.  How about if I start repairing you, so that when she gets back, we can set sail as soon as possible?"));
         lines.Add(new DialogueLine("Boat", "Repairing me?  What are you trying to say?!  "));
         lines.Add(new DialogueLine("Player", "Player: Well, uh..."));
-        lines.Add(new DialogueLine("Player", ">You look like you’ve seen better days."));
-		lines.Add(new DialogueLine("Player", "You look pretty bashed up.  I’m surprised you even made it to shore like this."));
-        lines.Add(new DialogueLine("Boat", "Oh yeah? Well you don’t look so great yourself!  "));
-        lines.Add(new DialogueLine("Player", "Hey!  For surviving a plane crash, I think I look all right!"));
-		lines.Add(new DialogueLine("Boat", "So your face looked like that before you crashed?"));
-        lines.Add(new DialogueLine("Player", "What’s that supposed to-"));
-        lines.Add(new DialogueLine("Player", " Look, instead of calling each other names, why don’t we try to get us and Captain Hornbeck off this island?"));
-		lines.Add(new DialogueLine("Player", ">Change the subject"));
-		lines.Add(new DialogueLine("Player", "Uh... hey!  Captain Hornbeck’s probably looking for supplies, right?"));
-		lines.Add(new DialogueLine("Boat", "So?"));
-		lines.Add(new DialogueLine("Player", "So, wouldn’t she be happy if she came back and you were ready to sail?  That would be a lot of stress off her mind!"));
-		lines.Add(new DialogueLine("Boat", "..."));
-		lines.Add(new DialogueLine("Boat", "Fine. ...do you know anything about carpentry?"));
-		lines.Add(new DialogueLine("Player", "...I made a spice rack in shop class?"));
-		lines.Add(new DialogueLine("Boat", "Ugh.  I suppose it can’t be helped.  I guess I can talk you through it."));
-		lines.Add(new DialogueLine("", "<Boat> taught me some of the basics of carpentry, and we started planning the repairs."));
-		
-
 
         return lines;
     }
+
+    private List<DialogueLine> RepairMilestone1_OptionA()
+    {
+        var lines = new List<DialogueLine>();
+
+        lines.Add(new DialogueLine("Player", "You look pretty bashed up.  I’m surprised you even made it to shore like this."));
+        lines.Add(new DialogueLine("Boat", "Oh yeah? Well you don’t look so great yourself!  "));
+        lines.Add(new DialogueLine("Player", "Hey!  For surviving a plane crash, I think I look all right!"));
+        lines.Add(new DialogueLine("Boat", "So your face looked like that before you crashed?"));
+        lines.Add(new DialogueLine("Player", "What’s that supposed to-"));
+        lines.Add(new DialogueLine("Player", " Look, instead of calling each other names, why don’t we try to get us and Captain Hornbeck off this island?"));
+
+        lines.AddRange(RepairMilestone1_Final());
+
+        return lines;
+    }
+
+    private List<DialogueLine> RepairMilestone1_OptionB()
+    {
+        var lines = new List<DialogueLine>();
+
+        lines.Add(new DialogueLine("Player", "Uh... hey!  Captain Hornbeck’s probably looking for supplies, right?"));
+        lines.Add(new DialogueLine("Boat", "So?"));
+        lines.Add(new DialogueLine("Player", "So, wouldn’t she be happy if she came back and you were ready to sail?  That would be a lot of stress off her mind!"));
+        lines.Add(new DialogueLine("Boat", "..."));
+
+        lines.AddRange(RepairMilestone1_Final());
+
+        return lines;
+    }
+
+    private List<DialogueLine> RepairMilestone1_Final()
+    {
+        var lines = new List<DialogueLine>();
+
+        lines.Add(new DialogueLine("Boat", "Fine. ...do you know anything about carpentry?"));
+        lines.Add(new DialogueLine("Player", "...I made a spice rack in shop class?"));
+        lines.Add(new DialogueLine("Boat", "Ugh.  I suppose it can’t be helped.  I guess I can talk you through it."));
+        lines.Add(new DialogueLine("", "<Boat> taught me some of the basics of carpentry, and we started planning the repairs."));
+
+        return lines;
+    }
+
     private List<DialogueLine> RepairMilestone2()
     {
         var lines = new List<DialogueLine>();
@@ -162,42 +204,105 @@ public class RepairBehavior : ActionBehavior
         lines.Add(new DialogueLine("Boat", "and that was the last time I saw her."));
         lines.Add(new DialogueLine("Boat", "She didn’t look back, she didn’t try to save me too... the trawler pulled away and I was alone in the storm..."));
 		lines.Add(new DialogueLine("", "wow... the captain abandoned <Boat>? What should I say?"));
-        lines.Add(new DialogueLine("", ">Hornbeck was a terrible captain"));
-        lines.Add(new DialogueLine("Player", "I can’t believe Hornbeck did that to you! A captain should always go down with her ship!"));
-		lines.Add(new DialogueLine("Boat", "It wasn’t her fault... I should have protected her in the storm, and I let her down..."));
-        lines.Add(new DialogueLine("Player", "A captain’s duty is to keep her boat safe, no matter what!  She could have had the trawler tow you to safety, but she left you behind.  You don’t deserve that!"));
-        lines.Add(new DialogueLine("", ">Hornbeck had to save herself"));
-		lines.Add(new DialogueLine("Player", "I can understand Hornbeck’s decision.  She wouldn’t have made it through the storm without signaling for help."));
-        lines.Add(new DialogueLine("Boat", "It’s my fault... I couldn’t protect her..."));
-        lines.Add(new DialogueLine("Player", "I’m sure it wasn’t personal!  A captain has to make those decisions.  I know she wouldn’t have abandoned you unless it was the only option."));
-		lines.Add(new DialogueLine("Boat", "I guess you’re right, <Player>..."));
-       
-
 
         return lines;
     }
-	private List<DialogueLine> RepairMilestone5()
+
+    private List<DialogueLine> RepairMilestone4_OptionA()
+    {
+        var lines = new List<DialogueLine>();
+
+        lines.Add(new DialogueLine("Player", "I can’t believe Hornbeck did that to you! A captain should always go down with her ship!"));
+        lines.Add(new DialogueLine("Boat", "It wasn’t her fault... I should have protected her in the storm, and I let her down..."));
+        lines.Add(new DialogueLine("Player", "A captain’s duty is to keep her boat safe, no matter what!  She could have had the trawler tow you to safety, but she left you behind.  You don’t deserve that!"));
+
+        lines.AddRange(RepairMilestone4_Final());
+
+        return lines;
+    }
+
+    private List<DialogueLine> RepairMilestone4_OptionB()
+    {
+        var lines = new List<DialogueLine>();
+
+        lines.Add(new DialogueLine("Player", "I can understand Hornbeck’s decision.  She wouldn’t have made it through the storm without signaling for help."));
+        lines.Add(new DialogueLine("Boat", "It’s my fault... I couldn’t protect her..."));
+        lines.Add(new DialogueLine("Player", "I’m sure it wasn’t personal!  A captain has to make those decisions.  I know she wouldn’t have abandoned you unless it was the only option."));
+
+        lines.AddRange(RepairMilestone4_Final());
+
+        return lines;
+    }
+
+    private List<DialogueLine> RepairMilestone4_Final()
+    {
+        var lines = new List<DialogueLine>();
+
+        lines.Add(new DialogueLine("Boat", "I guess you’re right, <Player>..."));
+
+        return lines;
+    }
+
+
+    private List<DialogueLine> RepairMilestone5()
     {
         var lines = new List<DialogueLine>();
 
         lines.Add(new DialogueLine("", "Just applying the finishing touches... <Boat> should be just about ready to set sail!"));
 		lines.Add(new DialogueLine("Player", "Have you been thinking about Captain Hornbeck?"));
 		lines.Add(new DialogueLine("Boat", "Yeah... do you think she made it to the mainland safely?"));
-		lines.Add(new DialogueLine("", ">Probably"));
-		lines.Add(new DialogueLine("Player", "I’m sure she made it back safely."));
-		lines.Add(new DialogueLine("", ">Probably not"));
-		lines.Add(new DialogueLine("Player", "You said the storm was pretty bad.  I’d be surprised if the trawler made it back."));
-		lines.Add(new DialogueLine("", ">Who cares?"));
-		lines.Add(new DialogueLine("Player", "Who cares?  She abandoned you.  Worrying about her is a waste of your time."));
-		lines.Add(new DialogueLine("Boat", "I guess you’re right."));
-		lines.Add(new DialogueLine("Boat", "I’ll still need a sail before we leave.  <Player>, do you think you’ll be able to work with this?"));
-		lines.Add(new DialogueLine("", "<Boat> gives me a sewing kit."));
-		     
 		
 
         return lines;
     }
-	private List<DialogueLine> Ending1()
+
+    private List<DialogueLine> RepairMilestone5_OptionA()
+    {
+        var lines = new List<DialogueLine>();
+
+        lines.Add(new DialogueLine("Player", "I’m sure she made it back safely."));
+
+        lines.AddRange(RepairMilestone5_Final());
+
+        return lines;
+    }
+
+    private List<DialogueLine> RepairMilestone5_OptionB()
+    {
+        var lines = new List<DialogueLine>();
+
+        lines.Add(new DialogueLine("Player", "You said the storm was pretty bad.  I’d be surprised if the trawler made it back."));
+
+        lines.AddRange(RepairMilestone5_Final());
+
+        return lines;
+    }
+
+    private List<DialogueLine> RepairMilestone5_OptionC()
+    {
+        var lines = new List<DialogueLine>();
+
+        lines.Add(new DialogueLine("Player", "Who cares?  She abandoned you.  Worrying about her is a waste of your time."));
+
+        lines.AddRange(RepairMilestone5_Final());
+
+        return lines;
+    }
+
+    private List<DialogueLine> RepairMilestone5_Final()
+    {
+        var lines = new List<DialogueLine>();
+
+        lines.Add(new DialogueLine("Boat", "I guess you’re right."));
+        lines.Add(new DialogueLine("Boat", "I’ll still need a sail before we leave.  <Player>, do you think you’ll be able to work with this?"));
+        lines.Add(new DialogueLine("", "<Boat> gives me a sewing kit."));
+
+        return lines;
+    }
+
+
+
+    private List<DialogueLine> Ending1()
     {
         var lines = new List<DialogueLine>();
 
